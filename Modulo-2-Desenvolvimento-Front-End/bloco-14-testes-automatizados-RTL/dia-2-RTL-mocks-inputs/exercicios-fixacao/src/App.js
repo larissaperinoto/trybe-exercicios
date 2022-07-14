fa3864dc6ab033ebbf16c3b6579c35d84499322d
Larissa Perinoto <perinotolarissa@gmail.com>
2022-07-14 15:41:02 -0300

    Exercícios de fixação do dia 14.2.

---
+++ Modulo-2-Desenvolvimento-Front-End/bloco-14-testes-automatizados-RTL/dia-2-RTL-mocks-inputs/exercicios-fixacao/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      joke: '',
+    };
+  }
+
+  componentDidMount() {
+    const API_URL = 'https://icanhazdadjoke.com/';
+    fetch(API_URL, { headers: { Accept: 'application/json' } })
+      .then((response) => response.json())
+      .then((data) => this.setState({ joke: data.joke }));
+  }
+
+  render() {
+    const { joke } = this.state;
+
+    return (
+      <div className="App">
+        {joke}
+      </div>
+    );
+  }
+}
+
+export default App;
