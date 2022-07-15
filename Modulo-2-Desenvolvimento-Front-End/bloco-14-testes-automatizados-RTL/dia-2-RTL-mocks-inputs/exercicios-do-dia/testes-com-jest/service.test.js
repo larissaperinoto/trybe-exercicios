67035953fd2a8a737bb1cba33572cd0be9175edd
Larissa Perinoto <perinotolarissa@gmail.com>
2022-07-15 12:04:35 -0300

    Exercício 3 com jest.

--- Modulo-2-Desenvolvimento-Front-End/bloco-14-testes-automatizados-RTL/dia-2-RTL-mocks-inputs/exercicios-do-dia/testes-com-jest/service.test.js
+++ Modulo-2-Desenvolvimento-Front-End/bloco-14-testes-automatizados-RTL/dia-2-RTL-mocks-inputs/exercicios-do-dia/testes-com-jest/service.test.js
@@ -18,3 +18,23 @@ test('Testa a função generateNumber com dois parametros', () => {
   expect(service.generateNumber(10, 2)).toBe(5);
   expect(service.generateNumber).toHaveBeenCalledTimes(2);
 });
+
+test('Testa a função generateNumber com três parametros', () => {
+  service.generateNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+
+  service.generateNumber(5, 2, 1);
+  expect(service.generateNumber).toHaveBeenCalled();
+  expect(service.generateNumber).toHaveBeenCalledTimes(1);
+  expect(service.generateNumber(5, 2, 1)).toBe(10);
+  expect(service.generateNumber).toHaveBeenCalledTimes(2);
+
+  service.generateNumber.mockReset();
+  expect(service.generateNumber).toHaveBeenCalledTimes(0);
+
+  service.generateNumber = jest.fn().mockImplementation((a) => a * 2);
+  service.generateNumber(2);
+  expect(service.generateNumber).toHaveBeenCalled();
+  expect(service.generateNumber).toHaveBeenCalledTimes(1);
+  expect(service.generateNumber(2)).toBe(4);
+  expect(service.generateNumber).toHaveBeenCalledTimes(2);
+});
