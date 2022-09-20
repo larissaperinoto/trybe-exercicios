e054365fe4a68f9e524bc30793b3531862a19a24
Larissa Perinoto <perinotolarissa@gmail.com>
2022-09-20 15:25:44 -0300

    [Dia 1] Exercício de fixação: aplicando condicionais com sql.

--- Modulo-3-Desenvolvimento-Back-End/bloco-21-funcoes-sql/index.sql
+++ Modulo-3-Desenvolvimento-Back-End/bloco-21-funcoes-sql/index.sql
@@ -14,3 +14,25 @@ SELECT substring('A linguagem JavaScript está entre as mais usadas', 13, 10);
 
 --Usando LCASE
 SELECT LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
+
+
+/* Aplicando Condicionais com SQL */
+
+-- Usando o IF para apenas uma condição
+
+SELECT film_id, title,
+IF(title = 'ACE GOLDFINGER', 'Já assisti a esse filme', 'Não conheço o filme')
+AS 'conheço o filme?'
+FROM sakila.film;
+
+-- Usando o CASE para mais de uma condição
+
+SELECT title, rating,
+	CASE
+    WHEN rating = 'G' THEN 'Livre para todos'
+    WHEN rating = 'PG' THEN 'Não recomendado para menores de 10 anos'
+    WHEN rating = 'PG-13' THEN 'Não recomendado para menores de 13 anos'
+    WHEN rating = 'R' THEN 'Não recomendado para menores de 17 anos'
+    ELSE 'Proibido para menores de idade'
+END AS 'público-alvo'
+FROM sakila.film;
