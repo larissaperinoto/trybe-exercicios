60d61858fe2e803ecbdd28f1fb9e9ce7bd558586
Larissa Perinoto <perinotolarissa@gmail.com>
2022-09-20 15:06:43 -0300

    [ dia 1] Exercício de fixação: manipulando strings.

---
+++ Modulo-3-Desenvolvimento-Back-End/bloco-21-funcoes-sql/index.sql
@@ -0,0 +1,16 @@
+/* Manipulando strings - Exercícios de fixação */
+
+-- Usando Uppercase
+SELECT UCASE('Trybe');
+
+-- Usando replace
+SELECT REPLACE('Você já ouviu falar do DuckDuckGo?', 'DuckDuckGo', 'Google');
+
+-- Usando CHAR_LENGTH
+SELECT character_length('Uma frase qualquer');
+
+-- Usando SUBSTRING
+SELECT substring('A linguagem JavaScript está entre as mais usadas', 13, 10);
+
+--Usando LCASE
+SELECT LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
