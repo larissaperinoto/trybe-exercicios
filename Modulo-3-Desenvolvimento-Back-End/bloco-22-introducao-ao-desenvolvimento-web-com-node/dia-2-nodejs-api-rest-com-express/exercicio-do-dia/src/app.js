d51965aa7401c04d183033738c000353b094f798
Larissa Perinoto <perinotolarissa@gmail.com>
2022-09-29 07:54:57 -0300

    [Dia 2] Exercício do dia.

---
+++ Modulo-3-Desenvolvimento-Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node/dia-2-nodejs-api-rest-com-express/exercicio-do-dia/src/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const data = require('./data');
+console.log(data)
+
+const app = express();
+
+app.use(express.json());
+
+app.get('', (_req, res) =>  res.status(200).json({ message: "Hello World!"}));
+
+app.get('/myActivities/:id', (req, res) => {
+  const { id } = req.params;
+  const response  = data.find(({ id }) => id === Number(id));
+  res.status(200).json(response);
+});
+
+app.get('/myActivities', (_req, res) =>  res.status(200).json({ data: data }));
+
+app.get('/filter/myActivities', (req, res) => {
+  const { status } = req.query;
+
+  let newData = data;
+
+  if (status) {
+    newData = data.filter((item) = item.status === status);
+  }
+
+  res.status(200).json({ data: newData });
+
+});
+
+module.exports = app;
--- Modulo-3-Desenvolvimento-Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node/dia-2-nodejs-api-rest-com-express/exercicios-aula/src/server.js
+++ Modulo-3-Desenvolvimento-Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node/dia-2-nodejs-api-rest-com-express/exercicios-aula/src/server.js
@@ -1,3 +1,3 @@
 const app = require('./app');
 
-app.listen(3001, () => console.log('server running on port 3001'));
+app.listen(3001, () => console.log('Servidor online na porta 3001'));
