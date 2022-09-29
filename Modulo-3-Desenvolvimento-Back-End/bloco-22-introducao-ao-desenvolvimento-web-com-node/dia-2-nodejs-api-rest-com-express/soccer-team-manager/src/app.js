3dbdd7a9604d98cca7672d24a63e5196eb1136f5
Larissa Perinoto <perinotolarissa@gmail.com>
2022-09-28 21:55:55 -0300

    [Dia 2] - Exercício de fixação concluído.

--- Modulo-3-Desenvolvimento-Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node/dia-2-nodejs-api-rest-com-express/soccer-team-manager/src/app.js
+++ Modulo-3-Desenvolvimento-Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node/dia-2-nodejs-api-rest-com-express/soccer-team-manager/src/app.js
@@ -15,6 +15,8 @@ const teams = [
 
 const app = express(); // inicia o programa com a função express.
 
+app.use(express.json());
+
 app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
 
 app.get('/teams', (req, res) => res.status(200).json({ teams }));
@@ -24,6 +26,39 @@ app.post('/teams', (req, res) => {
   teams.push(newTeam);
 
   res.status(201).json({ team: newTeam });
+});
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
+
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+  let updatedTeam;
+
+  for (let i = 0; i < teams.length; i += 1) {
+    const team = teams[i];
+
+    if (team.id === Number(id)) {
+      team.name = name;
+      team.initials = initials;
+      updatedTeam = team;
+    }
+  }
+
+  res.status(200).json({ updatedTeam });
+});
+
+app.delete('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const arrayPosition = teams.findIndex((team) => team.id === Number(id));
+  teams.splice(arrayPosition, 1);
+
+  res.status(200).end();
 });
 
 module.exports = app;
