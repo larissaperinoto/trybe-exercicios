88b4c0451b4a57d19fd10192f36ba52ded1ebf84
Larissa Perinoto <perinotolarissa@gmail.com>
2022-10-07 15:51:41 -0300

    [Exercício trybecar] - Cria conexão com o banco.

---
+++ Modulo-3-Desenvolvimento-Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-node/dia-5-nodejs-express-e-mysql/exercicio-fixacao/trybecash-api/src/model/db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql/promise');
+
+const connection = mysql.createPolol({
+  host: 'localhost',
+  port: 33060,
+  user: 'root',
+  password: 'root',
+  database: 'trybecashdb',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = connection;
